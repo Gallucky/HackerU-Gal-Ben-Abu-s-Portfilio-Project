69030901ee04eb485586a03482c8f2373ef5058e
Gallucky <galbenabu1111@gmail.com>
2024-11-25 23:27:01 +0200

    Setting up the space for world clocks project

--- JS Projects/tic_tac_toe/scripts/main-logic.js
+++ JS Projects/tic_tac_toe/scripts/main-logic.js
@@ -4,7 +4,7 @@
 // Currently the borders visuals are fully supported from 1-11,
 // 1 is just right border.
 // From 12 onwards there might be overlapping.
-const board = new Board(12);
+const board = new Board(13);
 
 // Different ways to print the board.
 console.log(board);
