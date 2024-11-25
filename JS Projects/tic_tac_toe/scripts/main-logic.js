aba1007d8d758917021acdc882af9f5cbea03118
Gallucky <galbenabu1111@gmail.com>
2024-11-25 17:39:42 +0200

    Current Progress + setting up files for lesson #9

---
+++ JS Projects/tic_tac_toe/scripts/main-logic.js
@@ -0,0 +1,11 @@
+// The main logic.
+
+// The tic tac toe board is an array of strings.
+// Currently the borders visuals are fully supported from 1-11,
+// 1 is just right border.
+// From 12 onwards there might be overlapping.
+const board = new Board(12);
+
+// Different ways to print the board.
+console.log(board);
+board.log();
