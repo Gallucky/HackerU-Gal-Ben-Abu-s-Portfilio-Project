635323e60fe03722803d59c0bca6a07bcfe563a7
Gallucky <galbenabu1111@gmail.com>
2024-12-07 21:23:02 +0200

    Math Game - Current Progress: Mostly linked starting screen and the questions screen

--- JS Projects/js_dom_mission_math_game/scripts/game_logic.js
+++ JS Projects/js_dom_mission_math_game/scripts/game_logic.js
@@ -39,4 +39,75 @@ const resetToStartingScreen = () => {
 };
 
 // Resetting to the Starting Screen.
-// resetToStartingScreen();
+resetToStartingScreen();
+
+const startGame = () => {
+    const gameContainer = document.querySelector(".game-container");
+
+    gameContainer.innerHTML = `<div class="game-toolbar">
+                <div class="tool-info-difficulty-level">
+                    <label for="difficulty" id="difficulty-label"> Difficulty: </label>
+                    <select class="difficulty easy-difficulty" name="difficulty" id="difficulty">
+                        <option class="easy-difficulty" value="easy">Easy 😊</option>
+                        <option class="medium-difficulty" value="medium">Medium 🤘</option>
+                        <option class="hard-difficulty" value="hard">Hard 🔥</option>
+                        <option class="extreme-difficulty" value="extreme">Extreme 🚒</option>
+                    </select>
+                </div>
+                <div class="tool-info-operator">
+                    <label for="operator-selection" id="operator-selection-label">
+                        Operator(s):
+                    </label>
+                    <select name="operator-selection" id="operator-selection">
+                        <option value="add">Addition ( + )</option>
+                        <option value="subtraction">Subtraction ( - )</option>
+                        <option value="multiplication">Multiplication ( × )</option>
+                        <option value="division">Division ( ÷ )</option>
+                        <option value="random">Random [+,-,×,÷]</option>
+                    </select>
+                </div>
+            </div>
+            <div class="game-body">
+                <div class="question">X OPERATOR Y = ?</div>
+
+                <div class="answer">
+                    <div class="answer-input">
+                        <label for="answer-question">Answer:</label>
+                        <input type="number" name="answer-question" id="answer" />
+                    </div>
+                    <button onclick="submitAnswer()" id="submit-answer">Submit</button>
+                </div>
+            </div>
+            <div class="game-information">
+                <table class="information-table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Question</th>
+                            <th>Difficulty</th>
+                            <th>Answer</th>
+                            <th>Your Answer</th>
+                            <th>Points Given</th>
+                        </tr>
+                    </thead>
+                    <tbody id="answers-for-previous-questions">
+                        <tr>
+                            <td>x</td>
+                            <td>x</td>
+                            <td>x</td>
+                            <td>x</td>
+                            <td>x</td>
+                            <td>x</td>
+                        </tr>
+                        <tr>
+                            <td>x</td>
+                            <td>x</td>
+                            <td>x</td>
+                            <td>x</td>
+                            <td>x</td>
+                            <td>x</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>`;
+};
